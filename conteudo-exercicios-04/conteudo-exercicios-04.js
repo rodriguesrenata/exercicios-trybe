96351b9ce00780d9a82c71cf97798a1850831644
rodriguesrenata <renata.jose@gmail.com>
2023-05-06 08:42:43 -0300

    organizacao das pastas e exercicios dia 4

---
+++ conteudo-exercicios-04/conteudo-exercicios-04.js
@@ -0,0 +1,15 @@
+let diasDaSemana = {
+    1: 'domingo',
+    2: 'segunda-feira',
+    3: 'terça-feira',
+    4: 'quarta-feira',
+    5: 'quinta-feira',
+    6: 'sexta-feira',
+    7: 'sábado',
+  };
+  
+  //diasDaSemana.1; // SyntaxError: Unexpected number
+  console.log(diasDaSemana['1']); // domingo
+
+
+  
---
+++ exercicios_dia_01.js
@@ -0,0 +1,17 @@
+
+
+let x = 60;
+let y = 60;
+let z = 60;
+
+let triangulo = -2;
+
+
+
+if (x+y+z === 180) {
+    console.log(true);
+} else if (triangulo <0);
+    console.log("erro");
+} else {
+    console.log(false);
+}
